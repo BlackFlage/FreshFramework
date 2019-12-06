2c62c43c75def7604005bc4e3d8e650191eddaff
Jakub Bednarek <BlackFlage10@gmail.com>
2019-12-06 22:30:48 +0100

    Changed grid behaviour to store apple and snake and control them

--- Engine/Grid.cpp
+++ Engine/Grid.cpp
@@ -1,6 +1,15 @@
 #include "Grid.h"
 
-void Grid::Draw(Snake& snake, Graphics & gfx)
+Grid::Grid(int& step)
+	:
+	snake({gridWidth / 2, gridHeight / 2}, Colors::Blue),
+	apple(Colors::Yellow),
+	step(step)
+{
+	apple.Spawn(gridWidth, gridHeight);
+}
+
+void Grid::Draw(Graphics & gfx)
 {
 
 	for (int i = 0; i < snake.GetActualSegments(); i++)
@@ -14,4 +23,37 @@ void Grid::Draw(Snake& snake, Graphics & gfx)
 	}
 
 	gfx.DrawRectangle(snake.GetPosition().x * tilesWidth, snake.GetPosition().y * tilesHeight, tilesWidth, tilesHeight, snake.GetHeadColor());
+	gfx.DrawRectangle(apple.GetPosition().x * tilesWidth, apple.GetPosition().y * tilesHeight, tilesWidth, tilesHeight, apple.GetColor());
+}
+
+void Grid::Update(char input)
+{
+	snake.SetDelta(input);
+
+	if (!gameOver && !snake.LoseCondition(gridWidth, gridHeight))
+	{
+		if (snake.CollidingWithApple(apple))
+		{
+			apple.Spawn(gridWidth, gridHeight);
+			snake.Grow();
+
+			score++;
+
+			if (score % 3 == 0)
+			{
+				step -= 1;
+			}
+		}
+
+		snake.Update();
+	}
+	else
+	{
+		gameOver = true;
+	}
+}
+
+bool Grid::GameOver() const
+{
+	return gameOver;
 }
--- Engine/Grid.h
+++ Engine/Grid.h
@@ -1,14 +1,23 @@
 #pragma once
 #include "Graphics.h"
 #include "Snake.h"
+#include "Apple.h"
 
 class Grid
 {
 public:
-	void Draw( Snake& snake, Graphics & gfx);
+	Grid(int& step);
+	void Draw(Graphics & gfx);
+	void Update(char input);
+	bool GameOver() const;
 private:
+	int& step;
+	int score = 0;
+	bool gameOver = false;
 	static constexpr int tilesWidth = 40;
 	static constexpr int tilesHeight = 40;
-	static constexpr int girdWidth = Graphics::ScreenWidth / tilesWidth;
+	static constexpr int gridWidth = Graphics::ScreenWidth / tilesWidth;
 	static constexpr int gridHeight = Graphics::ScreenHeight / tilesHeight;
+	Snake snake;
+	Apple apple;
 };
