121f1a49c9bd22e6828c0fae992dd66af1ed4fee
Jakub Bednarek <BlackFlage10@gmail.com>
2019-12-06 22:29:46 +0100

    Minor improvements to apple spawn and gettors

--- Engine/Apple.cpp
+++ Engine/Apple.cpp
@@ -11,10 +11,20 @@ void Apple::Spawn(int gridWidth, int gridHeight)
 	std::random_device rd{};
 	std::mt19937 rng(rd());
 
-	std::uniform_int_distribution<int> xDist(0, gridWidth);
-	std::uniform_int_distribution<int> yDist(0, gridHeight);
+	std::uniform_int_distribution<int> xDist(0, gridWidth - 1);
+	std::uniform_int_distribution<int> yDist(0, gridHeight - 1);
 
 	Vector2 newPosition = Vector2(xDist(rng), yDist(rng));
 
 	position = newPosition;
+}
+
+Color Apple::GetColor() const
+{
+	return color;
+}
+
+Vector2 Apple::GetPosition() const
+{
+	return position;
 }
--- Engine/Apple.h
+++ Engine/Apple.h
@@ -8,6 +8,8 @@ class Apple
 public:
 	Apple(Color c);
 	void Spawn(int gridWidth, int gridHeight);
+	Color GetColor() const;
+	Vector2 GetPosition() const;
 private:
 	Color color;
 	Vector2 position = { 0, 0 };
